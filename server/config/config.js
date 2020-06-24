bd3b226f498724a8a91d47cfae4db9f212d4d803
Alex Sanzana <alexsanzana1@gmail.com>
2020-06-24 13:44:58 -0400

    Subir variables de conexion ocultas en heroku

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'deb') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://userAtlas:Pass_123456@cluster0-dy9mc.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
